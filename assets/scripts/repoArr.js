095c1e919ad93096be0f53ab91515bd9c41a8e23
Drew A <alvarez.drew@gmail.com>
2020-11-06 21:34:11 -0600

    index page add with carousel with js, should have pushed earlier

---
+++ assets/scripts/repoArr.js
@@ -0,0 +1,46 @@
+let portfolioArr = [
+    {
+        appName: "Movie Binge",
+        imgName: "movieBinge",
+        appUrl: "https://drewalvarez.github.io/Project-Movie-Binge/",
+        appRepo: "https://github.com/DrewAlvarez/Project-Movie-Binge"
+    },
+    {
+        appName: "Weather Dashboard",
+        imgName: "weatherDashboard",
+        appUrl: "https://drewalvarez.github.io/HW-6-Weather-Dashboard/",
+        appRepo: "https://github.com/DrewAlvarez/HW-6-Weather-Dashboard"
+    },
+    {
+        appName: "Code Quiz",
+        imgName: "codeQuiz",
+        appUrl: "https://drewalvarez.github.io/HW-4-Code-Quiz/",
+        appRepo: "https://github.com/DrewAlvarez/HW-4-Code-Quiz/"
+    },
+    {
+        appName: "Work Day Scheduler",
+        imgName: "workDayScheduler",
+        appUrl: "https://drewalvarez.github.io/HW-5-Work_Day_Scheduler/",
+        appRepo: "https://github.com/DrewAlvarez/HW-5-Work_Day_Scheduler/"
+    },
+    {
+        appName: "Password Generator",
+        imgName: "pwGenerator",
+        appUrl: "https://drewalvarez.github.io/HW-3_Password-Generator/",
+        appRepo: "https://github.com/DrewAlvarez/HW-3_Password-Generator/"
+    },
+    {
+        appName: "Refractor/Accessibility Assignment",
+        imgName: "hwRefractor",
+        appUrl: "https://drewalvarez.github.io/HW-1-Refractory-Homework/",
+        appRepo: "https://github.com/DrewAlvarez/HW-1-Refractory-Homework/"
+    },
+    {
+        appName: "Template Engine",
+        imgName: "teamImg",
+        appUrl: "#",
+        appRepo: "https://github.com/DrewAlvarez/HW-10-Template-Engine"
+    }
+];
+
+module.exports = portfolioArr;
---
+++ assets/scripts/script.js
@@ -0,0 +1,53 @@
+const portArr = [
+    {
+        appName: "Movie Binge",
+        imgName: "movieBinge",
+        appUrl: "https://drewalvarez.github.io/Project-Movie-Binge/",
+        appRepo: "https://github.com/DrewAlvarez/Project-Movie-Binge"
+    },
+    {
+        appName: "Pizza Claw",
+        imgName: "pizzaClaw",
+        appUrl: "https://pizzaclawdrew.herokuapp.com/",
+        appRepo: "https://github.com/Feda2020/Project2"
+    },
+    {
+        appName: "Coming Soon",
+        imgName: "project3temp",
+        appUrl: "#",
+        appRepo: "#"
+    }
+]
+const carousel = $(".carousel-inner")
+
+for(var i = 0; i < portArr.length; i++){
+    let carouselItem = $("<div>");
+    let carouselImage = $("<img>");
+
+    if(i===0){
+        carouselItem.attr("class", "carousel-item active")
+    }else{
+        carouselItem.attr("class", "carousel-item")
+    }
+
+    
+    carouselImage.attr("src", "assets/img/"+ portArr[i].imgName + ".png");
+    carouselImage.attr("class", "d-block w-100 carouselImg")
+    carouselItem.append(carouselImage)
+    carousel.append(carouselItem)
+    let title = portArr[i].appName
+    let appBtn = portArr[i].appUrl
+    let repoBtn = portArr[i].appRepo
+    let imgEl = portArr[i].imgName
+
+    carouselImage.on("click", function(){
+        $(".modal-title").text(title)
+        $("#modalImg").attr("src", "assets/img/"+ imgEl + ".png")
+        $("#repoBtn").attr("href", repoBtn)
+        $("#appBtn").attr("href", appBtn)
+        $(".modal").modal("show")
+
+    })
+
+
+}
