039e8700a2f9c006150c3f912b8f63f55d0cff98
Andrew <alvarez.drew@gmail.com>
2020-11-09 15:04:45 -0600

    added other pages, copied some code from previous portflio page, worked on css

--- assets/scripts/portfolio.js
+++ assets/scripts/portfolio.js
@@ -6,6 +6,12 @@ let portfolioArr = [
         appRepo: "https://github.com/DrewAlvarez/Project-Movie-Binge"
     },
     {
+        appName: "Pizza Claw",
+        imgName: "pizzaClaw",
+        appUrl: "https://pizzaclawdrew.herokuapp.com/",
+        appRepo: "https://github.com/Feda2020/Project2"
+    },
+    {
         appName: "Weather Dashboard",
         imgName: "weatherDashboard",
         appUrl: "https://drewalvarez.github.io/HW-6-Weather-Dashboard/",
@@ -41,6 +47,33 @@ let portfolioArr = [
         appUrl: "#",
         appRepo: "https://github.com/DrewAlvarez/HW-10-Template-Engine"
     }
-];
+]
 
-module.exports = portfolioArr;
+var imageBank = $("#image-bank");
+
+for(var i = 0; i < portfolioArr.length; i++){
+    var linkEl = $("<div>").addClass("imageHov");
+    var imageEl = $("<img>");
+
+    linkEl.attr("href", portfolioArr[i].appUrl);
+    imageEl.attr("class", "imgSize");
+    imageEl.attr("src", "assets/img/"+ portfolioArr[i].imgName + ".png");
+    linkEl.attr("title", portfolioArr[i].appName)
+    linkEl.append(imageEl);
+    imageBank.append(linkEl);
+    let title = portfolioArr[i].appName
+    let appBtn = portfolioArr[i].appUrl
+    let repoBtn = portfolioArr[i].appRepo
+    let imgEl = portfolioArr[i].imgName
+
+    imageEl.on("click", function(){
+        $(".modal-title").text(title)
+        $("#modalImg").attr("src", "assets/img/"+ imgEl + ".png")
+        $("#repoBtn").attr("href", repoBtn)
+        $("#appBtn").attr("href", appBtn)
+        $(".modal").modal("show")
+
+    })
+
+
+}
